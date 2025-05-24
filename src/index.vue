b085f0d10818bb4e1e56a6cc05df0f16553bb9c5
Td62 <tudou666666a@outlook.com>
2025-05-24 14:52:01 +0800

    page base html

---
+++ src/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <Header></Header>
+    <Body></Body>
+    <Footer></Footer>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Body from "./components/Body.vue";
+import Footer from "./components/Footer.vue";
+export default{
+    components:{
+        Header:Header,
+        Body:Body,
+        Footer:Footer
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import Button from './components/Button.vue'
 
 
 import { createApp, VueElement } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from './index.vue'
 const app = createApp(App)
 
 app.component('MyButton',Button)
